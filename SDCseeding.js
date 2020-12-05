810759df7491d7a3e105af08d60a01da25970358
Ricky Marasigan <rickymarasigan@Rickys-MacBook-Pro.local>
2020-12-04 20:35:03 -0800

    completed a working rough draft of SDC seeding script

--- SDCseeding.js
+++ SDCseeding.js
@@ -1,28 +1,29 @@
 const fs = require('fs');
 const csvWriter = require('csv-write-stream');
-var writer = csvWriter();
-var faker = require('faker');
+const listingsWriter = csvWriter();
+const customersWriter = csvWriter();
+const reviewsWriter = csvWriter();
+const reviewsByCustomerWriter = csvWriter();
+const faker = require('faker');
+const debug = require('debug')('app:gen:psql');
 
-var generateListings = () => {
-    writer.pipe(fs.createWriteStream('csvData/listings.csv'));
-    
+const generateListings = () => {
+    listingsWriter.pipe(fs.createWriteStream('csvData/listings.csv'));
     for (let i = 1; i <= 10; i++) {
-        writer.write({
+        listingsWriter.write({
             listing_id: i,
             addres: `${faker.address.streetAddress()} St. ${faker.address.city()}, ${faker.address.state()}, ${faker.address.zipCode()}`,
             owner: `${faker.name.firstName()} ${faker.name.lastName()}`
         });
     }
-
-    writer.end();
-    console.log('generated listings');
+    listingsWriter.end();
+    console.log('generated listings');    
 }
 
 const generateCustomers = () => {
-    writer.pipe(fs.createWriteStream('csvData/customers.csv'));
-
+    customersWriter.pipe(fs.createWriteStream('csvData/customers.csv'));
     for (let i = 1; i <= 10; i++) {
-        writer.write({
+        customersWriter.write({
             customer_id: i,
             name: `${faker.name.firstName()} ${faker.name.lastName()}`,
             email: faker.internet.email(),
@@ -30,19 +31,18 @@ const generateCustomers = () => {
             listing_id: Math.floor(Math.random() * (10 - 1) + 1)
         });
     }
-
-    writer.end();
+    customersWriter.end();
     console.log('generated customers');
 }
 
 const generateReviews = () => {
-    writer.pipe(fs.createWriteStream('csvData/reviews.csv'));
+    reviewsWriter.pipe(fs.createWriteStream('csvData/reviews.csv'));
     var range = {
         'min': 1,
         'max': 5
     };
     for (let i = 1; i <= 10; i++) {
-        writer.write({
+        reviewsWriter.write({
             review_id: i,
             posting_date: faker.date.past(1),
             text: faker.lorem.sentences(),
@@ -54,19 +54,18 @@ const generateReviews = () => {
             value: faker.random.number(range)
         });
     }
-
-    writer.end();
+    reviewsWriter.end();
     console.log('generated reviews');
 }
 
 const generateReviewsByCustomer = () => {
-    writer.pipe(fs.createWriteStream('csvData/reviewsByCustomer.csv'));
+    reviewsByCustomerWriter.pipe(fs.createWriteStream('csvData/reviewsByCustomer.csv'));
     var range = {
         'min': 1,
         'max': 5
     };
     for (let i = 1; i <= 10; i++) {
-        writer.write({
+        reviewsByCustomerWriter.write({
             review_id: i,
             customer_id: i,
             posting_date: faker.date.past(1),
@@ -79,16 +78,16 @@ const generateReviewsByCustomer = () => {
             value: faker.random.number(range)
         });
     }
-
-    writer.end();
+    reviewsByCustomerWriter.end();
     console.log('generated reviews by customer');
 }
 
-generateListings();
-generateCustomers();
-generateReviews();
-generateReviewsByCustomer();
+async function dataGenerator() {
+    debug('start');
+    await generateListings();
+    await generateCustomers();
+    await generateReviews();
+    await generateReviewsByCustomer();
+}
 
-/*
-COPY reviews (review_id, posting_date, text, cleanliness, communication, check_in, accuracy, location, value) FROM '/Users/rickymarasigan/Desktop/SDCseedTesting/test.csv' WITH header = true;
-*/
+dataGenerator();
