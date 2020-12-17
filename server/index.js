d8df75348ad3ca8929b7f340a32ba08c07598cc4
Ricky Marasigan <rickymarasigan@Rickys-MacBook-Pro.local>
2020-12-16 20:54:39 -0800

    removed mongoose connection

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const port = 3003;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost/FEC', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost/FEC', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // app.use(morgan('dev'));
 
